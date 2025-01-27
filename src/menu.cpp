1ce2253578870b7f1005875a725dd853b1632d40
Kevin Juselius <keffi_jv89@hotmail.com>
2025-01-27 16:15:30 +0100

    v1

--- src/menu.cpp
+++ src/menu.cpp
@@ -7,19 +7,14 @@
 void menu(){
     std::string choice;
     while(true){
-        std::cout << std::endl << "1. Create user" << std::endl;
-        std::cout << "2. Test login" << std::endl;
-        std::cout << "3. Exit" << std::endl;
+        std::cout << std::endl << "1. Enter your hash" << std::endl;
+        std::cout << "2. Exit" << std::endl;
         std::cout << "Choice: ";
         std::getline(std::cin, choice);
         
         if(choice == "1"){
-            createUser();
         }
         else if(choice == "2"){
-            testLogin;
-        }
-        else if(choice == "3"){
             return;
         }
         else{
